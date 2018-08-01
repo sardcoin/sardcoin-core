cc9a2589beb7749f4af1d6bab9f8d6552f309224
Ibbus93 <ibbus93@gmail.com>
2018-08-01 11:41:05 +0200

    Updating coupons API

--- schemas/coupons-schema.js
+++ schemas/coupons-schema.js
@@ -4,14 +4,14 @@ const createCouponSchema = {
     query: {},
     body: {
         title:          Joi.string().allow('').min(5).max(50).required(),
-        description:    Joi.string().min(5).max(255).allow('').allow(null),
-        price:          Joi.number().integer().required(),
+        description:    Joi.string().min(5).max(255).allow(null),
+        price:          Joi.number().required(),
         valid_from:     Joi.number(),
-        valid_until:    Joi.number(),
+        valid_until:    Joi.number().allow(null),
         state:          Joi.number().integer(),
-        constraints:    Joi.string().allow(''),
+        constraints:    Joi.string().allow(null),
         owner:          Joi.number().integer().required(),
-        consumer:       Joi.number().integer()
+        consumer:       Joi.number().integer().allow(null)
     },
     params: {}
 };
