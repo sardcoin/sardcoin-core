df8307ced790dd9e20e9a291e67976591fb54a00
Sebastian Podda <sebastianpodda@unica.it>
2018-08-31 17:09:26 +0200

    fixed upload error

--- config/express.js
+++ config/express.js
@@ -16,7 +16,7 @@ const bodyParser =   require('body-parser');
 module.exports = function (app, passport) {
 
     const corsOpt = {
-        origin: 'http://localhost:4200',
+        origin: ['http://localhost:4200', 'http://localhost:443', 'http://platform.sardcoin.eu'],
         credentials: true,
         methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], // to works well with web app, OPTIONS is required
         allowedHeaders: ['Content-Type', 'Authorization'] // allow json and token in the headers
@@ -44,8 +44,8 @@ module.exports = function (app, passport) {
     app.use(express.static(config.root + '/media/images'));
 
     // enabling cors
-    //app.use(cors(corsOpt)); // cors for all the routes of the application
-    //app.options('*', cors(corsOpt)); // automatic cors gen for HTTP verbs in all routes, This can be redundant but I kept to be sure that will always work.
-    app.use(cors());
+    app.use(cors(corsOpt)); // cors for all the routes of the application
+    app.options('*', cors(corsOpt)); // automatic cors gen for HTTP verbs in all routes, This can be redundant but I kept to be sure that will always work.
+    //app.use(cors());
 
 };
