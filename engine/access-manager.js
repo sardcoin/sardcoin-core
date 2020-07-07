f83ff1529380f8537105cf8091ad492eb281d0d7
StefanoChessa <sr.chessa@tiscali.it>
2020-07-07 17:03:32 +0200

    Fix for consumer registration

--- engine/access-manager.js
+++ engine/access-manager.js
@@ -36,7 +36,6 @@ exports.createUser = function (req, res, next) {
                     });
                 } else {
                     // A new user can be created
-
                     Users.create({
                         username: user.username,
                         email: user.email,
@@ -45,7 +44,7 @@ exports.createUser = function (req, res, next) {
                         first_name: user.first_name,
                         last_name: user.last_name,
                         birth_place: user.birth_place,
-                        birth_date: new Date(user.birth_date),
+                        birth_date: user.birth_date || user.birth_date !== '' ? new Date(user.birth_date) : null,
                         fiscal_code: user.fiscal_code,
                         address: user.address,
                         province: user.province,
