60f224d53fe6c9d2947230c237d95d793ccf991e
StefanoChessa <sr.chessa@tiscali.it>
2020-07-13 17:12:58 +0200

    Minor fix

--- engine/coupon-manager.js
+++ engine/coupon-manager.js
@@ -503,7 +503,7 @@ const buyCoupons = async (req, res) => {
                 }
             }
 
-            await BlockchainManager.buyBlockchainCoupon(req.user.id, order_list);
+            //await BlockchainManager.buyBlockchainCoupon(req.user.id, order_list);
 
         } catch (e) {
             console.error(e);
@@ -536,7 +536,7 @@ const buyCoupons = async (req, res) => {
             // The purchase is done
             await unlockTables();
             order_id = await OrdersManager.createOrderFromCart(req.user.id, order_list);
-
+            await BlockchainManager.buyBlockchainCoupon(req.user.id, order_list);
 
             return res.status(HttpStatus.OK).send({
                 success: true,
